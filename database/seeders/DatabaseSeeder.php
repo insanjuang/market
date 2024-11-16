283f9ec1a0ae4772a7640bf83223cff18d4ddf82
agusanaluam <sugaanaluam@gmail.com>
2024-11-16 23:14:52 +0700

    update store and dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,9 +17,13 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@market.com',
+            'email_verified_at' => date("Y-m-d H:i:s"),
+            'password' => Hash::make('admin'),
+            'foto' => '',
+            'level' => 1,
+        ]);
     }
 }
